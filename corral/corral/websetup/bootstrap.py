bb03564c206e8c7de5adb9bdf5a169455b5e19b7
dollydhariwal <ddhariwal@gmail.com>
2015-01-29 19:07:30 -0800

    Adding a new application Corral

---
+++ corral/corral/websetup/bootstrap.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Setup the corral application"""
+from __future__ import print_function
+
+import logging
+from tg import config
+from corral import model
+
+def bootstrap(command, conf, vars):
+    """Place any commands to setup corral here"""
+
+    # <websetup.bootstrap.before.auth
+
+    # <websetup.bootstrap.after.auth>
